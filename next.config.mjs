439bf8e83590ac53e212f667a27329b80bbc1f32
Rudr <rudrprasad@yahoo.com>
2024-12-09 11:26:39 +1200

    docker init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    outputStandalone: true,
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
