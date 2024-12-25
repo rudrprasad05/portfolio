5188db4eb9071145b67d778ad1d1b7d4b9f7f16d
Rudr <rudrprasad@yahoo.com>
2024-12-26 00:10:22 +1200

    bug fixes

--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -2,7 +2,7 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { NextRequest, NextResponse } from "next/server";
 
 const s3Client = new S3Client({
-  region: process.env.AWS_S3_REGION ?? "",
+  region: process.env.AWS_S3_REGION ?? "us-east-1",
   credentials: {
     accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID ?? "",
     secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY ?? "",
